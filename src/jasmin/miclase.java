15fe07bca68049ba95d7b1c6dd33044644310863
carolinavasquez <deysicarolina@unitec.com>
2014-03-18 23:51:57 -0600

    Update miclase.java

--- src/jasmin/miclase.java
+++ src/jasmin/miclase.java
@@ -7,36 +7,39 @@ public class miclase
 
 	public static void main(String[] args)
 	{
-		Scanner miscanner=new Scanner(System.in);
-		System.out.print("ingrese un numero");
-		int num1=miscanner.nextInt();
-		System.out.print("ingrese otro numero");
-		int num2=miscanner.nextInt();
-		System.out.print("ingrese 1 para +; 2 para -; 3 para *; 4 para /");
-		int num3=miscanner.nextInt();
-		{
-		if(num3==1)
-
-		System.out.print("suma es:");
-		System.out.println(num1+num2);
-		}
-		{
-		if(num3==2)
-		System.out.print("resta es:");
-		System.out.println(num1-num2);
-		}
-		{
-		if(num3==3)
-		System.out.print("multiplicacion es:");
-		System.out.println(num1*num2);
-		}
-		{
-		if(num3==4)
-		System.out.print("division es:");
-		System.out.println(num1/num2);
-		}
-	}
-}
-
+	
+                int num1,num2,suma,resta,multi,division,n;
+		Scanner mi_escaner = new Scanner (System.in);
+		System.out.print("Ingrese primer numero:"); 
+		num1=mi_escaner.nextInt();
+		System.out.print("Ingrese segundo numero:"); 
+		num2=mi_escaner.nextInt();
+		System.out.println("Mostrar Opcion:"); 
+		System.out.println("1:"); 
+		System.out.println("2:"); 
+		System.out.println("3:"); 
+		System.out.println("4:"); 
+		n=mi_escaner.nextInt(); 
+		switch (n){ 
+		case 1:
+			suma=num1+num2; 
+			System.out.println("La suma es"+suma); 
+			break; 
+			case 2: 
+				resta=num1-num2;
+				System.out.println("La resta es"+resta);
+            break; 
+            case 3:
+            	multi=num1*num2; 
+            System.out.println("La multiplicacion es"+multi); 
+            break; 
+            case 4: 
+            	division=num1/num2; 
+            	System.out.println("La division es"+division);
+            	break; 
+            
+            	} 
+		} 
+	} 
 
 
